ee96f84733913119019df076a9aff756acd2a8bd
Taras <t.kulias.m@gmail.comqwqq>
2022-08-13 19:22:35 +0200

    Switch calc-1

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-function calc(operator, a, b) {
+function calc(operator, operand1, operand2) {
   switch (operator) {
-    case "add":
-      return a + b;
-    case "multi":
-      return a * b;
-    case "subtract":
-      return a - b;
+    case 'add':
+      return operand1 + operand2;
+      break;
+    case 'multi':
+      return operand1 * operand2;
+      break;
+    case'subtract':
+      return operand1 - operand2;
+      break
     default:
-      console.log("Ошибка");
+      console.log('Error');
   }
 }
-calc("add", 3, 2);
+
+console.log(calc('subtract',12,5))
