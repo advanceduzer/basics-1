93777fb21fffeb46e50ee86cd9289af4229880e8
PolinaMulya <muliukovap@gmail.com>
2022-08-14 16:38:21 +0500

    New operators

--- polinamulya/calculator2_switch.js
+++ polinamulya/calculator2_switch.js
@@ -1,16 +1,16 @@
 let firstOperand = prompt('Введите первое значение: ', 0);
 let secondOperand = prompt('Введите второе значение: ', 0);
-let operator = prompt('Введите оператор: ', 'add, multi, substract');
+let operator = prompt('Введите оператор: ', '+, *, -');
 
 alert('Результат: '+ firstOperand + operator + secondOperand + '='+ calc());
 
 function calc(){
     switch(operator){
-        case 'add':
+        case '+':
             return +firstOperand + +secondOperand;
-        case 'multi':
+        case '*':
             return firstOperand*secondOperand;
-        case 'substract':
+        case '-':
             return firstOperand-secondOperand;
         default:
             alert('Ошибка! Попробуйте еще раз!');
