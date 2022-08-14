dc1001be58c54f6bf157a140c3e0b74f3b9849db
Aziza Kadyrova <azziza.kadyrova@gmail.com>
2022-08-15 00:01:09 +0600

    first

---
+++ calctest/script.js
@@ -0,0 +1,51 @@
+// function calc(a, b, action) {
+//   let result
+//   if (action == '+') {
+//       result = a + b;
+//   } else if (action == '-') {
+//       result = a - b;
+//   } else if (action == '*') {
+//       result = a * b;
+//   }
+//   return result;
+
+// }
+
+// console.log(calc(5, 7, '+'));
+// console.log(calc(5, 7, '-'));
+// console.log(calc(5, 7, '*'));
+
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// switch (number){
+// case 0:  
+// alert ('Вы ввели число 0');
+// break;
+// case 1:
+// alert ('Вы ввели число 1');
+// break;
+// case 2:
+// alert ('Вы ввели число 2, а может и 3');
+// break;
+// case 3:
+// alert ('Вы ввели число 2, а может и 3');
+// }
+
+
+   
+
+//   function calc(operation, a, b) {
+//     switch(operation) {
+//         case 'add' : return a + b;
+//         break;
+//         case 'multi' : return a * b;
+//         break;
+//         case 'subtract' : return a - b;
+//         break
+//     }
+// }
+//     console.log(calc('add', 1, 2));
+//     console.log(calc('multi', 1, 2));
+//     console.log(calc('subtract', 3, 2));
+
+
