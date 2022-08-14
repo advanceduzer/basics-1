6b4fcb553017ad6e458e0e993f7d29df7de3fd4b
d33sf0me <katekrokh@gmail.com>
2022-08-14 18:48:39 +0300

    practice-1

---
+++ d33sf0me/calc.js
@@ -0,0 +1,14 @@
+function func(operation, num1, num2) {
+  let result;
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+  }
+}
