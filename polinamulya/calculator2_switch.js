b3cb2178bd863f3c9f09f12cc7c73f225b04380c
PolinaMulya <muliukovap@gmail.com>
2022-08-14 14:21:09 +0500

    Loading file..

---
+++ polinamulya/calculator2_switch.js
@@ -0,0 +1,18 @@
+let firstOperand = prompt('Введите первое значение: ', 0);
+let secondOperand = prompt('Введите второе значение: ', 0);
+let operator = prompt('Введите оператор: ', 'add, multi, substract');
+
+alert('Результат: '+ firstOperand + operator + secondOperand + '='+ calc());
+
+function calc(){
+    switch(operator){
+        case 'add':
+            return +firstOperand + +secondOperand;
+        case 'multi':
+            return firstOperand*secondOperand;
+        case 'substract':
+            return firstOperand-secondOperand;
+        default:
+            alert('Ошибка');
+    }
+}
