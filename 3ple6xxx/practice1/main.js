8e7afc434fe8a42cc99b6ef94e950228c7337264
Roman <mikhailovromaaan@gmail.com>
2022-08-14 16:48:28 +0200

    practice1

---
+++ 3ple6xxx/practice1/main.js
@@ -0,0 +1,13 @@
+function calc(value,a,b){
+switch (value) {
+    case 'add':
+    return +a + +b;
+    case 'multi':
+    return a * b;
+    case 'subtract':
+    return a - b;
+}
+};
+console.log(calc('add',1,2));
+console.log(calc('multi',1,2));
+console.log(calc('subtract',3,2));
