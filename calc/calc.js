563448c0f53d778046326289b783858df2aa2288
Zemskov Alexey <zemskv@yandex.ru>
2022-08-14 18:30:22 +0300

    add calc.js

---
+++ calc/calc.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return a + b;
+}
+function multi(a, b) {
+  return a * b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+
+function Calc(operation = "add", a, b) {
+  const isNumber = !isNaN(a) && !isNaN(b) ? true : false;
+
+  if (isNumber) {
+    switch (operation) {
+      case "add":
+        return add(a, b);
+      case "multi":
+        return multi(a, b);
+      case "subtract":
+        return subtract(a, b);
+      default:
+        return "Операция не определена";
+    }
+  } else {
+    return "Ошибка в аргументе";
+  }
+}
+
+console.log(Calc("multi", 5, 3));
