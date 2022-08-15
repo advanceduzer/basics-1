cbbc1846b2e7ff71d9baa608fba4cb2ba943483b
PankJK <daidengy@yandex.ru>
2022-08-15 13:58:24 +0300

    second

--- pankJK/calc.js
+++ pankJK/calc.js
@@ -1,15 +1,16 @@
 function calc(func, a, b) {
-    switch (func) {
-      case "add":
-        return +a + +b;
-      case "multi":
-        return a * b;
-      case "sub":
-       console.log("sub")
-        return a - b;
-  
-      default:
-        return "error";
-    }
+  switch (func) {
+    case "add":
+      return +a + +b;
+    case "multi":
+      return a * b;
+    case "subtract":
+      return a - b;
+
+    default:
+      return "error";
   }
-  console.log(calc("sub", 1, 2));
+}
+console.log(calc("add", 1, 2));
+console.log(calc("multi", 1, 2));
+console.log(calc("subtract", 3, 2));
