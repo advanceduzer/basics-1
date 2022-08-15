1af8ee3ddcb6e1151f1df5f1f454857ffeef9edb
Agitobl <vlad.agito@gmail.com>
2022-08-15 12:30:44 +0300

    task 15

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ function calc(operator, a, b) {
   }
 }
 calc("add", 3, 2);
+
+/////////////////////////////////
+
+for (let i = 1; i <= 19; i++) {
+  console.log(i);
+}
+
+/////////////////////////////////
+
+let i = 1;
+
+while(i <= 19) {
+  console.log(i);
+  i++;
+}
+
+
