0d049a746ae49c5a86f03e31a036dfe280e0b784
Advanceduzer <advanceduzer@gmail.com>
2022-09-08 01:42:47 +0200

    calculator UI

---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    function calc() {
+        
+        let a = document.getElementsByTagName("input")[0].value;
+        let action = document.getElementsByTagName("select")[0].value;
+        let b = document.getElementsByTagName("input")[1].value; 
+        
+        const operations = {
+            '+': +a + +b,
+            '*': a * b,
+            '-': a - b,
+            '/': (a / b).toFixed(2),
+        }
+
+        for (let key in operations) {
+            if (key == action) {
+                return operations[key];
+            }
+        }
+    
+    }
+
+    let elements = document.querySelectorAll('#calculator');
+    for (let elem of elements) {
+        elem.onchange = function(event) {
+            document.getElementById("result").innerHTML=`${calc()}`;     
+        };
+    }
+
+});
