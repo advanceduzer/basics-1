6d8166aa6e30235e8acdf080c0adf8873173107c
Advanceduzer <advanceduzer@gmail.com>
2022-09-08 02:04:46 +0200

    деление на ноль

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+
     function calc() {
         
         let a = document.getElementsByTagName("input")[0].value;
         let action = document.getElementsByTagName("select")[0].value;
         let b = document.getElementsByTagName("input")[1].value; 
+
+        if (b == 0 && action == '/'){
+            alert('На ноль делить нельзя')
+        } 
         
         const operations = {
             '+': +a + +b,
             '*': a * b,
             '-': a - b,
-            '/': (a / b).toFixed(2),
+            '/':  (a / b).toFixed(2),
+
         }
 
         for (let key in operations) {
@@ -24,8 +30,12 @@ document.addEventListener("DOMContentLoaded", () => {
     let elements = document.querySelectorAll('#calculator');
     for (let elem of elements) {
         elem.onchange = function(event) {
-            document.getElementById("result").innerHTML=`${calc()}`;     
+            document.getElementById("result").innerHTML=`${calc()}`;   
         };
+        elem.onkeyup = function(event) {
+            document.getElementById("result").innerHTML=`${calc()}`;   
+        };
+
     }
 
 });
